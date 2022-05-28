fbd0eac46b75fb643eab2f7853e8e9f4f90f2859
Harsimran Kaur <harsimran.kaur1@whitehatjr.com>
2022-05-28 22:11:28 +0530

    Initial Commit

---
+++ LoginSwiftui/LoginSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSwiftuiApp.swift
+//  LoginSwiftui
+//
+//  Created by ITRS-1519 on 28/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
